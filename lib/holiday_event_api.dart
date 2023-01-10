fb86cdce5ab91f4e8b74fc68ca268502dc3968a9
Seth Westphal <westy92@gmail.com>
2023-01-09 20:01:28 -0600

    Fix docs.

--- lib/holiday_event_api.dart
+++ lib/holiday_event_api.dart
@@ -4,3 +4,18 @@
 library holiday_event_api;
 
 export 'src/holiday_event_api.dart';
+
+// models
+export 'src/model/alternate_name.dart';
+export 'src/model/event_info.dart';
+export 'src/model/event_summary.dart';
+export 'src/model/founder_info.dart';
+export 'src/model/get_event_info_response.dart';
+export 'src/model/get_events_response.dart';
+export 'src/model/image_info.dart';
+export 'src/model/occurrence.dart';
+export 'src/model/pattern.dart';
+export 'src/model/rate_limit.dart';
+export 'src/model/rich_text.dart';
+export 'src/model/search_response.dart';
+export 'src/model/standard_response.dart';
